fa8f39c1feb1d9c866e9e4a6dc155292389c41e0
geynske006 <jimmygeyns@gmail.com>
2013-12-09 23:13:19 +0100

    First upload: construction page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PixelIglo</title>
+	<link rel="stylesheet" href="./css/pxl.css">
+	<link rel="icon" 
+      type="image/png" 
+      href="./pic/fav.png">
+</head>
+<body>
+	<div id="container">
+		<img src="./pic/logo.png" alt="logo van PixelIglo">
+		 <div id="baseline">We're still building our webIglo!</div>
+	</div>
+</body>
+</html>
