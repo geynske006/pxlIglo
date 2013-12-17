7a82309df3354e8fd508b313e5873555b43ebfce
root <root@ubuntu.(none)>
2013-12-17 20:00:46 +0100

    social add - webUnderConstruction

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>PixelIglo</title>
+	<link rel="stylesheet" href="http://reset5.googlecode.com/hg/reset.min.css">
 	<link rel="stylesheet" href="./css/pxl.css">
 	<link rel="icon" 
       type="image/png" 
@@ -12,6 +13,10 @@
 	<div id="container">
 		<img src="./pic/logo.png" alt="logo van PixelIglo">
 		 <div id="baseline">We're still building our webIglo!</div>
+		 <div id="social">
+			<a href="http://www.twitter.com"><img src="./pic/t.png" alt="twitter link"></a>
+			<a href="http://www.facebook.com"><img src="./pic/t.png" alt="facebook link"></a>
+		 </div>
 	</div>
 </body>
 </html>
